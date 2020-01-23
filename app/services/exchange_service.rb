d780038510b8c6fc0fcb2b0d761d5ec3543a0829
Ricardo Moraes <r.a.moraess@gmail.com>
2020-01-23 01:46:55 -0300

    include rubocop

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -1,24 +1,24 @@
+# frozen_string_literal: true
+
 require 'rest-client'
 require 'json'
- 
+
 class ExchangeService
   def initialize(params = {})
     @params = params
   end
- 
+
   def convert
-    begin
-      api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
-      api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
+    api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
+    api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
 
-      response = RestClient.get("#{api_url}?token=#{api_key}&currency=#{@params[:from]}/#{@params[:to]}")
+    response = RestClient.get("#{api_url}?token=#{api_key}&currency=#{@params[:from]}/#{@params[:to]}")
 
-      result = JSON.parse(response.body)['currency'][0]['value'].to_f
-      
-      result * @params[:amount]
+    result = JSON.parse(response.body)['currency'][0]['value'].to_f
 
-    rescue RestClient::ExceptionWithResponse => e
-      e.response
-    end
+    result * @params[:amount]
+
+  rescue RestClient::ExceptionWithResponse => e
+    e.response
   end
 end
